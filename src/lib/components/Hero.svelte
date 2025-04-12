0b543bb9fcedd6a4728460cd4ad3490a90d395d4
sagarmish1234@gmail.com <sagarmish1234@gmail.com>
2025-04-12 12:39:18 +0530

    Added typing animation

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -1,21 +1,16 @@
 <script>
     import {GridAndDotBackgrounds} from "$lib/components/ui/GridAndDotBackground/index.js";
     import Header from "./Header.svelte"
-    // import Robot from "$lib/models/robot_playground/robot_playground.svelte"
     import Bot from "$lib/models/robot_playground/bot.svelte"
     import {Canvas} from "@threlte/core";
-    // import {OrbitControls} from '@threlte/extras'
     import {T} from '@threlte/core'
+    import TypewriterEffect from "$lib/components/ui/TypewriterEffect/TypewriterEffect.svelte";
 </script>
 
 <section>
 
-    <Header></Header>
+    <!--    <Header></Header>-->
     <GridAndDotBackgrounds>
-
-
-
-        <!--        Hero Section-->
         <Canvas>
             <T.AmbientLight color="#ffffff" intensity={0.2}/>
             <T.DirectionalLight
@@ -25,21 +20,57 @@
                     shadow.camera.top={8}
                     castShadow
             />
-            <!--            <Robot position={[1.75,.3,0]} scale={0.8}></Robot>-->
-            <Bot position={[30.75,-10.3,-100]} scale={0.25} rotation="{[0,-.35,0]}"></Bot>
+            <Bot position={[30.75,0.3,-100]} scale={0.25} rotation="{[0,-.35,0]}"></Bot>
             <T.PerspectiveCamera
                     makeDefault
                     position={[0, 1, 5]}
             >
-                <!--                <OrbitControls/>-->
             </T.PerspectiveCamera>
         </Canvas>
     </GridAndDotBackgrounds>
-    <section class="text-white">
-        <div>Hi! I am </div>
-        <div>Sagar Mishra</div>
-        <h1>Software Engineer</h1>
+    <!--    Jumbotron-->
+    <section class="text-white jetbrains-mono-400 w-full px-52 pt-24 flex flex-col gap-8 mt-32">
+        <div class="text-5xl ">Hello I am</div>
+        <div class="text-6xl">Sagar Mishra</div>
+        <TypewriterEffect></TypewriterEffect>
     </section>
 
 
 </section>
+
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,100..800;1,100..800&display=swap');
+    @import url('https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100..900;1,100..900&display=swap');
+    @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap');
+
+    .jetbrains-mono-400 {
+        font-family: "JetBrains Mono", monospace;
+        font-optical-sizing: auto;
+        font-weight: 400;
+        font-style: normal;
+    }
+
+
+    .raleway-400 {
+        font-family: "Raleway", sans-serif;
+        font-optical-sizing: auto;
+        font-weight: 400;
+        font-style: normal;
+    }
+
+    .montserrat-400 {
+        font-family: "Montserrat", sans-serif;
+        font-optical-sizing: auto;
+        font-weight: 400;
+        font-style: normal;
+    }
+
+
+    .montserrat-600 {
+        font-family: "Montserrat", sans-serif;
+        font-optical-sizing: auto;
+        font-weight: 600;
+        font-style: normal;
+    }
+
+</style>
---
+++ src/lib/components/ui/TypewriterEffect/TypewriterEffect.svelte
@@ -0,0 +1,24 @@
+<script>
+    import TypeIt from "typeit";
+    import {onMount} from "svelte";
+
+    let ref = $state();
+
+
+    onMount(() => {
+        new TypeIt(ref, {speed: 130})
+            .type("soft",{delay:500})
+            .move(-3)
+            .delete(1)
+            .type("S",{delay:100})
+            .move(3,{delay:300})
+            .type("ware Engineer")
+            .go();
+    })
+
+</script>
+
+
+<div bind:this={ref} class="text-emerald-600 text-6xl">
+
+</div>
