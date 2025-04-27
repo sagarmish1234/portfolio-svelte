49cba241750af49fbeeb7ce47fc70ff54db9ec9f
sagarmish1234@gmail.com <sagarmish1234@gmail.com>
2025-04-27 20:59:01 +0530

    got github pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,19 @@
-import { mdsvex } from 'mdsvex';
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import {mdsvex} from 'mdsvex';
+import adapter from '@sveltejs/adapter-static';
+import {vitePreprocess} from '@sveltejs/vite-plugin-svelte';
 
 const config = {
-	preprocess: [vitePreprocess(), mdsvex()],
-	kit: { adapter: adapter() },
-	extensions: ['.svelte', '.svx']
+    preprocess: [vitePreprocess(), mdsvex()],
+    kit: {
+        adapter: adapter({
+            fallback: '404.html'
+        }),
+        paths: {
+            base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+        }
+
+    },
+    extensions: ['.svelte', '.svx']
 };
 
 export default config;
